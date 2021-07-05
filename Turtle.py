afcb78f9163ca1f3e4287c632b6a843c18046504
chandana19698 <44301373+chandana19698@users.noreply.github.com>
2021-07-05 22:59:30 +0530

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -22,3 +22,31 @@ for _ in range(4):
     tim.pendown()
     tim.forward(20)
     tim.left(90)
+    
+    
+  # triangle to decagon
+
+import turtle as t
+from turtle import Turtle, Screen
+import random
+
+
+colors = ["forest green", "green yellow", "red", "medium spring green", "blue"]
+tim = t.Turtle()
+
+
+def draw_shape(num_of_sides):
+    angle = 360 / num_of_sides
+    for _ in range(num_of_sides):
+        t.forward(100)
+        t.right(angle)
+
+
+for i in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(i)
+
+
+screen = Screen()
+screen.exitonclick()
+
