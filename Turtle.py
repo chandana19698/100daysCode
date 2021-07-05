fc13d0b69fa65970c1ed3d25edb6d496b880a98c
chandana19698 <44301373+chandana19698@users.noreply.github.com>
2021-07-05 22:30:06 +0530

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -6,3 +6,19 @@ tim.color("DarkGreen")
 for _ in range(4):
     tim.forward(100)
     tim.left(90)
+    
+    
+    
+#Dotted line
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("DarkGreen")
+for _ in range(4):
+    tim.forward(20)
+    tim.penup()
+    tim.forward(20)
+    tim.pendown()
+    tim.forward(20)
+    tim.left(90)
