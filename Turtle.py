1596c2ee48e8315ac4d0618ecfae88397e001a3d
chandana19698 <44301373+chandana19698@users.noreply.github.com>
2021-07-08 07:57:10 +0530

    Update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -144,3 +144,43 @@ for i in range(1,number+1):
         tim.setheading(0)
 screen = Screen()
 screen.exitonclick()
+
+
+#Etch a sketch
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+tim.speed("fastest")
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def clear_screen():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+def turn_left():
+    new_heading=tim.heading() +10
+    tim.setheading(new_heading)
+
+
+def turn_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear_screen)
+screen.exitonclick()
